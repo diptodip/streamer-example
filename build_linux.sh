3af8dc25638dc90a90b5733c4e097375c5154e1c
Jinyao Yan <lingerlyn@example.com>
2022-07-23 17:04:01 -0400

    switch to docking

--- build_linux.sh
+++ build_linux.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 mkdir -p release;
 rm -f release/streamer_example;
-cp src/Roboto-Regular.ttf release/Roboto-Regular.ttf
-cp src/shader_picture.fs release/shader_picture.fs
-cp src/shader_picture.vs release/shader_picture.vs
+cp src/Roboto-Regular.ttf Roboto-Regular.ttf
+cp src/fa-solid-900.ttf fa-solid-900.ttf
 
 nvcc -c src/create_image_cuda.cu -arch=sm_80 -o release/create_image_cuda.o
 nvcc -c src/ColorSpace.cu -arch=sm_80 -o release/ColorSpace.o
@@ -27,9 +26,11 @@ g++ -Ofast -ffast-math -std=c++11 \
     -I/usr/local/cuda-11.4/include \
     -I$DIR_IMGUI \
     -I$DIR_IMGUI/backends \
+    -Ilib/IconFontCppHeaders \
     -L/usr/local/cuda-11.4/lib64/ -lcudart -lcuda -lnppicc -lnvcuvid \
     -lGLEW -lGLU -lGL \
     `pkg-config --static --libs glfw3` \
     `pkg-config --cflags libavformat libswscale libswresample libavutil libavcodec` \
     `pkg-config --libs libavformat libswscale libswresample libavutil libavcodec`
-    
+
+./release/streamer_example
--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@
 #include "Logger.h"
 #include "gl_helper.h"
 #include "decoder.h"
+#include "IconsFontAwesome5.h"
 
 
 #include <iostream>       // std::cout
@@ -51,98 +52,90 @@ simplelogger::Logger *logger = simplelogger::LoggerFactory::CreateConsoleLogger(
 int main(int, char**)
 {
 
-	ck(cuInit(0));
+    ck(cuInit(0));
     char szInFilePath[256] = "/home/jinyao/Videos/2022-07-08_17:37:58/Cam5.mp4";
-	// char szInFilePath[256] = "C:/Users/yaoyao/Videos/2022-03-13_12_13_07/Cam5.mp4";
+    // char szInFilePath[256] = "C:/Users/yaoyao/Videos/2022-03-13_12_13_07/Cam5.mp4";
 
-	int iGpu = 0;
+    int iGpu = 0;
     CUcontext cuContext = NULL;
     createCudaContext(&cuContext, iGpu, CU_CTX_SCHED_BLOCKING_SYNC);
     CheckInputFile(szInFilePath);
     std::cout << szInFilePath << std::endl;
 
 
-	// Setup window
-	glfwSetErrorCallback(glfw_error_callback);
-	if (!glfwInit())
-	    return 1;
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
 
-	// Decide GL+GLSL versions
+    // Decide GL+GLSL versions
 #if defined(__APPLE__)
-	// GL 3.2 + GLSL 150
-	const char* glsl_version = "#version 150";
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
 #else
-	// GL 3.0 + GLSL 130
-	const char* glsl_version = "#version 130";
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 #endif
 
-	// Create window with graphics context
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "Streamer Example", NULL, NULL);
-	if (window == NULL)
-	    return 1;
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1); // Enable vsync
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Streamer Example", NULL, NULL);
+    if (window == NULL)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
 
-	// Initialize OpenGL functions with GLEW
-	glew_error_callback(glewInit());
+    // Initialize OpenGL functions with GLEW
+    glew_error_callback(glewInit());
 
 
-    // Shader screenShader("shader_picture.vs", "shader_picture.fs");
-    // float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
-    //     // positions   // texCoords
-    //     -1.0f,  1.0f,  0.0f, 1.0f,
-    //     -1.0f, -1.0f,  0.0f, 0.0f,
-    //      1.0f, -1.0f,  1.0f, 0.0f,
 
-    //     -1.0f,  1.0f,  0.0f, 1.0f,
-    //      1.0f, -1.0f,  1.0f, 0.0f,
-    //      1.0f,  1.0f,  1.0f, 1.0f
-    // };
+ // ************* Dear Imgui ********************//
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+    // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 
-	// unsigned int quadVAO, quadVBO;
-    // glGenVertexArrays(1, &quadVAO);
-    // glGenBuffers(1, &quadVBO);
-    // glBindVertexArray(quadVAO);
-    // glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-    // glEnableVertexAttribArray(0);
-    // glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-    // glEnableVertexAttribArray(1);
-    // glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 
-    // screenShader.use();
-    // screenShader.setInt("screenTexture", 0);
 
-    // // framebuffer configuration
-    // // -------------------------
-    // unsigned int framebuffer;
-    // glGenFramebuffers(1, &framebuffer);
-    // glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-    // // create a color attachment texture
-    // unsigned int textureColorbuffer;
-    // glGenTextures(1, &textureColorbuffer);
-    // glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 3208, 2200, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+    // Setup Dear ImGui style
+    ImGui::StyleColorsClassic();
 
-    // if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-    //     std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
-    // glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Load a nice font
+    io.Fonts->AddFontFromFileTTF("Roboto-Regular.ttf", 15.0f);
+    // merge in icons from Font Awesome
+    static const ImWchar icons_ranges[] = { ICON_MIN_FA, ICON_MAX_16_FA, 0 };
+    ImFontConfig icons_config; icons_config.MergeMode = true; icons_config.PixelSnapH = true;
+    io.Fonts->AddFontFromFileTTF( FONT_ICON_FILE_NAME_FAS, 15.0f, &icons_config, icons_ranges);
+    // use FONT_ICON_FILE_NAME_FAR if you want regular instead of solid
 
 
     // Create a OpenGL texture identifier
     GLuint image_texture;
- 	glGenTextures(1, &image_texture);
+     glGenTextures(1, &image_texture);
     glBindTexture(GL_TEXTURE_2D, image_texture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 3208, 2200, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 3208, 2200, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
     // Setup filtering parameters for display
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -151,170 +144,118 @@ int main(int, char**)
 
 
 
-	// Setup Dear ImGui context
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
+    // Our state
+    ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);
+    double result = 0.0;
+    unsigned long long num_heads = 0;
+    unsigned long long num_tails = 0;
+    
+    
 
-	// Setup Dear ImGui style
-	ImGui::StyleColorsClassic();
-
-	// Setup Platform/Renderer backends
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init(glsl_version);
-
-	// Load a nice font
-	io.Fonts->AddFontFromFileTTF("Roboto-Regular.ttf", 15.0f);
-
-	// Our state
-	ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);
-	double result = 0.0;
-	unsigned long long num_heads = 0;
-	unsigned long long num_tails = 0;
-	
-	
-	// GLuint texture;
-	// GLuint pbo;
-	// cudaGraphicsResource_t cuda_resource = 0;
-	// unsigned char *cuda_buffer;
-	// size_t cuda_pbo_storage_buffer_size;
-	// create_texture(&texture);
-	// create_pbo(&pbo);
-	// bind_pbo(&pbo);
-	// register_pbo_to_cuda(&pbo, &cuda_resource);
-	// unbind_texture();
-	// unbind_pbo();
-
-
-	// decoding 
-    // unsigned char *display_buffer;
-	// int size_pic = 10586400 *  sizeof(unsigned char);
-    // cudaMalloc((void **)&display_buffer, size_pic);
-
-	int size_pic = 3208 * 2200 * 4 *  sizeof(unsigned char);
+    int size_pic = 3208 * 2200 * 4 *  sizeof(unsigned char);
     unsigned char* current_frame_on_host;
-	current_frame_on_host = (unsigned char*)malloc(size_pic); 
+    current_frame_on_host = (unsigned char*)malloc(size_pic); 
 
-	// start thread 
+    // start thread 
     std::vector<std::thread> decoder_threads;
-	bool* decoding_flag = new bool(true);
+    bool* decoding_flag = new bool(true);
     decoder_threads.push_back(std::thread(&decoder_process, szInFilePath, 1, current_frame_on_host, decoding_flag));
 
-	int frame_i = 0;
+    int frame_i = 0;
 
-	// Main loop
-	while (!glfwWindowShouldClose(window))
-	{
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
 
-		// Poll and handle events (inputs, window resize, etc.)
-		glfwPollEvents();
+        // Poll and handle events (inputs, window resize, etc.)
+        glfwPollEvents();
 
-		// // Create image on CUDA and transfer to PBO then OpenGL texture
-		// // CUDA-GL INTEROP STARTS HERE -------------------------------------------------------------------------
-		// map_cuda_resource(&cuda_resource);
-		// cuda_pointer_from_resource(&cuda_buffer, &cuda_pbo_storage_buffer_size, &cuda_resource);
-		// // copy to display buffer first 
-		// Nv12ToColor32<RGBA32>(display_buffer, dec.GetWidth(), cuda_buffer, nPitch, dec.GetWidth(), dec.GetHeight(), iMatrix);
-		// unmap_cuda_resource(&cuda_resource);
-
-		// // CUDA-GL INTEROP ENDS HERE ---------------------------------------------------------------------------
-		// bind_pbo(&pbo);
-		// bind_texture(&texture);
-		// upload_image_pbo_to_texture(); // Needs no arguments because texture and PBO are bound
-		// unbind_texture();
-		// unbind_pbo();
-
-		// glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-		// // make sure we clear the framebuffer's content
-		// glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		// glClear(GL_COLOR_BUFFER_BIT);
-
-		// screenShader.use();
-		// glBindVertexArray(quadVAO);
-		// glBindTexture(GL_TEXTURE_2D, texture);
-		// glDrawArrays(GL_TRIANGLES, 0, 6);
-		// glBindTexture(GL_TEXTURE_2D, 0);
-		// glBindVertexArray(0);
-		// glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-		bind_texture(&image_texture);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 3208, 2200, 0, GL_RGBA, GL_UNSIGNED_BYTE, current_frame_on_host);
-		unbind_texture();
+        bind_texture(&image_texture);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 3208, 2200, 0, GL_RGBA, GL_UNSIGNED_BYTE, current_frame_on_host);
+        unbind_texture();
 
 
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
-		// Start the Dear ImGui frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+        // Show a simple window that we create ourselves. This is
+        // just to show that tracking of state is working
+        // separately from CUDA.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+            ImGui::Begin("Hello, world!");
+            ImGui::Text("Flip a coin here!");
+            ImGui::SameLine();
+            if (ImGui::Button("Flip!")) {
+                result = ((double) rand() / (RAND_MAX));
+                if (result > 0.5) {
+                    num_heads++;
+                } else {
+                    num_tails++;
+                }
+            }
+            if (result > 0.5) {
+                ImGui::Text("Heads!");
+            } else {
+                ImGui::Text("Tails!");
+            }
+            if ((num_heads + num_tails) > 0) {
+                ImGui::Text("Proportion heads: %.3f", (float) num_heads / (num_heads + num_tails));
+            }
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
 
-		// Show a simple window that we create ourselves. This is
-		// just to show that tracking of state is working
-		// separately from CUDA.
-		{
-		static float f = 0.0f;
-		static int counter = 0;
-		ImGui::SetNextWindowSize(ImVec2(0, 0), 0); // Setting size to 0, 0 forces auto-fit
-		ImGui::Begin("Hello, world!");
-		ImGui::Text("Flip a coin here!");
-		ImGui::SameLine();
-		if (ImGui::Button("Flip!")) {
-			result = ((double) rand() / (RAND_MAX));
-			if (result > 0.5) {
-				num_heads++;
-			} else {
-				num_tails++;
-			}
-		}
-		if (result > 0.5) {
-			ImGui::Text("Heads!");
-		} else {
-			ImGui::Text("Tails!");
-		}
-		if ((num_heads + num_tails) > 0) {
-			ImGui::Text("Proportion heads: %.3f", (float) num_heads / (num_heads + num_tails));
-		}
-		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		ImGui::End();
-		}
+        // Render a video frame
+        {
+            ImGui::Begin("Hello, video!");
+            ImGui::Text("pointer = %p", image_texture);
+            ImVec2 avail_size = ImGui::GetContentRegionAvail();
+            ImGui::Image((void*)(intptr_t)image_texture, avail_size);
+            ImGui::End();
+        }
 
-		// Render a video frame
-		{
-		ImGui::SetNextWindowSize(ImVec2(0, 0), 0); // Setting size to 0, 0 forces auto-fit
-		ImGui::Begin("Hello, video!");
-		ImGui::Text("pointer = %p", image_texture);
-		ImGui::Image((void*)(intptr_t)image_texture, ImVec2(3208, 2200));
-		// ImGui::Text("pointer = %p", texture);
-		// ImGui::Image((void*)(intptr_t)texture, ImVec2(3208, 2200));
-		ImGui::End();
-		}
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        
+        
+        // Update and Render additional Platform Windows
+        // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+        //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
 
-		// Rendering
-		ImGui::Render();
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-		glfwSwapBuffers(window);
-		frame_i++;
-	}
+        glfwSwapBuffers(window);
+        frame_i++;
+    }
 
 
-	// Cleanup
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
-	*decoding_flag = false;
+    *decoding_flag = false;
     // wait for threads to join
     for (auto& t : decoder_threads)
         t.join();
 
-	return 0;
+    return 0;
 }
